6e9ee1f8a58fd97c3ee651cf155ec8596e8676ae
Anatoliy <a.bygyb@gmail.com>
2018-01-20 15:26:24 +0200

    test-epoch lohic

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/sendAnswers', function(req, res, next) {
+    let testID = req.body.testID;
+    let useerAnswers = req.body.userAnswers;
+    let currentTest = tests.find(x => x.testID === testID);
+    let rightAnswers = currentTest.rightAnswers;
+
+    function checkTest() {
+        let arrayOfScores = [];
+        useerAnswers.forEach(function(answer, i, arr) {
+            if (answer == rightAnswers[i]) {
+                arrayOfScores.push(true);
+            } else {
+                arrayOfScores.push(false);
+            }
+        });
+        return arrayOfScores;
+    }
+    let scoreResult = checkTest();
+    res.json(
+        {
+            'result': scoreResult,
+            'rightAnswers': rightAnswers
+        });
+});
+
+>>>>>>> Stashed changes
 module.exports = router;
